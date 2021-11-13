d89ed9bc4f36ecf346b7f07d40e4764be9e688a5
KrisnaDewa <59174531+krisdewa@users.noreply.github.com>
2021-11-13 21:16:39 +0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,14 +4,14 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "OTA5MDQwMDc4MjU5NjIxOTE4.YY-fiw.mktpppG34uzq8An7zuiOcYAYmzI", //Discord Bot Token
+  Token: process.env.Token || "OTA5MDQwMDc4MjU5NjIxOTE4.YY-fiw.h4ERbI8g8C_F_yFreFUBrU45Odo", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "909040078259621918", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "nF33K2CJnDFekseGVR_etnJ-fcI9PG-D", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
-  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  "24/7": true, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Pikachu is cute", //A Secret like a password
   IconURL: "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
@@ -27,10 +27,10 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
-    secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+    host: "lavalink-repl.krisdewa.repl.co",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "youshallnotpass",
+    secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
